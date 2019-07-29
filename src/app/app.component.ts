f17119071c5dec60ce2f904f04bcf0eb41dbb2f5
unknown <cb0806151@otc.edu>
2019-07-28 21:54:24 -0500

    upload project

---
+++ src/app/app.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from "@angular/core";
+import * as moment from "moment";
+import { isNavigationCancelingError } from '@angular/router/src/shared';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  verifyDeleteModalOpen = false;
+  removingCategoryId;
+  budgetTotal = 0.00
+  newEntry = "";
+  newCategory = "";
+  categories = []
+  entriesNegativeByDefault = false;
+  cantSubmitCharactersErrorSignVisible = false;
+
+  addCategory() {
+    if (this.newCategory.length > 0) {
+      this.categories.unshift(
+        {
+          "name": this.newCategory,
+          "budget_entries": [],
+          "entry_holder": "",
+          "entries_total": 0.00,
+          "entries_visible": true
+        }
+      )
+      this.newCategory = ""
+    }
+  }
+
+  addEntry(categoryIndex) {
+    if (this.categories[categoryIndex].entry_holder.length > 0) {
+      let entry = parseFloat(this.categories[categoryIndex].entry_holder);
+
+      if (isNaN(entry)) {
+        this.cantSubmitCharactersErrorSignVisible = true;
+        return;
+      } else {
+        this.cantSubmitCharactersErrorSignVisible = false;
+      }
+    
+      if (this.categories[categoryIndex].entry_holder.length > 0 &&
+          this.entriesNegativeByDefault && 
+          this.categories[categoryIndex].entry_holder.indexOf("+") == -1 &&
+          entry > 0) {
+        entry = entry * -1;
+      }
+      this.budgetTotal += entry;
+      this.categories[categoryIndex].entries_total += entry;
+      this.categories[categoryIndex].budget_entries.push(
+        {
+          "value": entry.toFixed(2)
+        }
+      )
+      this.categories[categoryIndex].entry_holder = ""
+    }
+  }
+
+  removeEntry(categoryIndex, entriesIndex) {
+    let entry = this.categories[categoryIndex].budget_entries[entriesIndex].value;
+    this.budgetTotal -= entry;
+    this.budgetTotal = parseFloat(this.budgetTotal.toFixed(2));
+    this.categories[categoryIndex].entries_total -= entry;
+    this.categories[categoryIndex].budget_entries.splice(entriesIndex, 1);
+  }
+
+  removeCategory() {
+    var component = this;
+    this.categories[this.removingCategoryId].budget_entries.forEach(function(entry){
+      component.budgetTotal -= entry.value;
+    })
+    this.budgetTotal = parseFloat(this.budgetTotal.toFixed(2));
+    this.categories.splice(this.removingCategoryId, 1);
+  }
+}
