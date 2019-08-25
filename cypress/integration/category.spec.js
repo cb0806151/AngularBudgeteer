b96d728567c7d6c8603b054ee874f11de8a050da
cb0806151 <cb0806151@otc.edu>
2019-08-25 17:45:46 -0500

    create a test for category functionality

---
+++ cypress/integration/category.spec.js
@@ -0,0 +1,10 @@
+describe('Test category functionality', () => {
+
+    it('create a new category', () => {
+        cy.visit('https://cb0806151.github.io/AngularBudgeteer/')
+        cy.get('.budget_app_input').eq(0).type('Food')
+        cy.get('.budget_app_input_btn').eq(0).click()
+        cy.get('.budget_app_input').eq(1).should('have.attr', 'placeholder', 'Enter a entry for Food here')
+    })
+
+})
