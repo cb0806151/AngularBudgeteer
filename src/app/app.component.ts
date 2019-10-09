411691ab77e4359b3c1e590b5010a84f11d684bb
cb0806151 <cb0806151@otc.edu>
2019-10-08 21:22:29 -0500

    test against localhost

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import * as moment from "moment";
 import { isNavigationCancelingError } from '@angular/router/src/shared';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EmailService } from './app.service';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +18,19 @@ export class AppComponent {
   categories = []
   entriesNegativeByDefault = false;
   cantSubmitCharactersErrorSignVisible = false;
+
+  messageReceiver = "holycow.baunach@gmail.com";
+  messageSubject = "Rapscallion!";
+  messageBody = "Pistols at dawn!!!";
+
+  constructor(
+    private emailSender: EmailService) {}
+
+  sendEmail() {
+    this.emailSender.sendEmail(this.messageReceiver, this.messageSubject, this.messageBody).subscribe(result => {
+      console.log(result)
+    })
+  }
 
   addCategory() {
     if (this.newCategory.length > 0) {
